9a9008e9337d6a187d4798eb0d470346f02058ce
Thanh Luu <congthanhluu@gmail.com>
2024-03-30 20:51:30 +0700

    testing pipeline

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -13,6 +13,15 @@ def login (user, password):
     driver = webdriver.Chrome()
     print ('Browser started successfully. Navigating to the demo page to login.')
     driver.get('https://www.saucedemo.com/')
-
+    input_username = driver.find_element(By.ID, 'user-name')
+    input_password = driver.find_element(By.ID, 'password')
+    btn_login = driver.find_element(By.ID, 'login-button')
+    
+    input_password.send_keys(user)
+    input_password.send_keys(password)
+    btn_login.click()
+    product_label = driver.find_element(By.CSS_SELECTOR, "div[class='inventory_item_name']").text
+    assert "Sauce Labs Bike Light" in product_label
+    
 login('standard_user', 'secret_sauce')
 

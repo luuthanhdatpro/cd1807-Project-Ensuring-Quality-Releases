2fef315c557ea2048dc2cce40148e4b2aeb103ce
Thanh Luu <congthanhluu@gmail.com>
2024-04-21 10:57:04 +0700

    testing pipeline

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -10,18 +10,34 @@ def login (user, password):
     # options = ChromeOptions()
     # options.add_argument("--headless") 
     # driver = webdriver.Chrome(options=options)
-    driver = webdriver.Chrome()
-    print ('Browser started successfully. Navigating to the demo page to login.')
-    driver.get('https://www.saucedemo.com/')
-    input_username = driver.find_element(By.ID, 'user-name')
-    input_password = driver.find_element(By.ID, 'password')
-    btn_login = driver.find_element(By.ID, 'login-button')
+    # driver = webdriver.Chrome()
+    # print ('Browser started successfully. Navigating to the demo page to login.')
+    # driver.get('https://www.saucedemo.com/')
+    # input_username = driver.find_element(By.ID, 'user-name')
+    # input_password = driver.find_element(By.ID, 'password')
+    # btn_login = driver.find_element(By.ID, 'login-button')
     
-    input_password.send_keys(user)
-    input_password.send_keys(password)
-    btn_login.click()
-    product_label = driver.find_element(By.CSS_SELECTOR, "div[class='inventory_item_name']").text
-    assert "Sauce Labs Bike Light" in product_label
+    # input_password.send_keys(user)
+    # input_password.send_keys(password)
+    # btn_login.click()
+    # product_label = driver.find_element(By.CSS_SELECTOR, "div[class='inventory_item_name']").text
+    # assert "Sauce Labs Bike Light" in product_label
+    options = ChromeOptions()
+    options.add_argument('--no-sandbox')
+    options.add_argument("--headless") 
+    options.add_argument("--remote-debugging-port=9222")
+    driver = webdriver.Chrome(options=options)
+    
+    # Test Login to the site
+    print('Browser started. Access to the demo page to login.')
+    driver.get('https://www.saucedemo.com/')
+    driver.find_element(By.CSS_SELECTOR,"input[id='user-name']").send_keys(user)
+    driver.find_element(By.CSS_SELECTOR,"input[id='password']").send_keys(password)
+    driver.find_element(By.CSS_SELECTOR,"input[id='login-button']").click()
+    path_content_div = "div[id='page_wrapper'] > div[id='contents_wrapper']"
+    results = driver.find_element(By.CSS_SELECTOR,path_content_div + " > div[class='header_container'] > div[class='header_secondary_container'] > span").text
+    assert "Products" in results
+    print("Successfully logged in as " + user)    
     return driver
 def add_to_cart(driver, items):
     print ('Adding 6 items to cart ...')
--- terraform/modules/appservice/appservice.tf
+++ terraform/modules/appservice/appservice.tf
@@ -2,7 +2,7 @@ resource "azurerm_service_plan" "test" {
   name                = "${var.application_type}-${var.resource_type}"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group_name}"
-  os_type             = "Linux"
+  os_type             = "Window"
   sku_name            = "F1"
 }
 
@@ -17,5 +17,9 @@ resource "azurerm_linux_web_app" "test" {
   }
   site_config {
     always_on = false
+    application_stack{
+      dotnet_version = "v4.0"
+    }
   }
+
 }

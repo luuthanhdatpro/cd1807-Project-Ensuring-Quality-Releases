8fc5e67858775f643169fc470ae4308084ad3557
Thanh Luu <congthanhluu@gmail.com>
2024-04-21 14:00:14 +0700

    testing pipeline

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -16,7 +16,7 @@ def login(user, password):
     options = ChromeOptions()
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
-    options.addArguments("--disable-dev-shm-usage")
+    options.add_argument("--disable-dev-shm-usage")
     driver = webdriver.Chrome(options=options)
     driver.get('https://www.saucedemo.com/')
 
